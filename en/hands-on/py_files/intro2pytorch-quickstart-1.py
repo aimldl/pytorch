47c5a88491890771cda101f33bf375364fc64ca2
aimldl <mrtonnet@gmail.com>
2021-04-26 13:24:17 +0900

    Update intro2pytorch-quickstart-1.py

--- en/hands-on/py_files/intro2pytorch-quickstart-1.py
+++ en/hands-on/py_files/intro2pytorch-quickstart-1.py
@@ -73,12 +73,12 @@ def train(dataloader, model, loss_fn, optimizer):
         # Compute prediction error
         pred = model(X)
         loss = loss_fn(pred, y)
-    
+
         # Backpropagation
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    
+
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
@@ -96,7 +96,7 @@ def test(dataloader, model):
     test_loss /= size
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-    
+
 epochs = 5
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
